ad905d6e4cf100644ad3ea301f491b476c7b929b
Rick Kersmarki <rkersmarki@gmail.com>
2022-08-02 11:52:24 -0500

    switch added

--- Calculator exercise/Program.cs
+++ Calculator exercise/Program.cs
@@ -1,46 +1,55 @@
-﻿
-List<string> numbers = new() { "1", "2", "3" };
+﻿List<string> numbers = new() { "4", "2", "1" };
 
-List<int> newNumbers = numbers.ConvertAll<int>(Convert.ToInt32);
+List<double> newNumbers = numbers.ConvertAll<double>(Convert.ToDouble);
 
-foreach (int number in newNumbers)
-{
-    Console.WriteLine(number);
-}
+Console.WriteLine(String.Join(", ", newNumbers));
 
-//int product = 1;
+Console.WriteLine();
 
-//foreach(int number in newNumbers)
-//{
-//    product *= number;
-//}
+Console.WriteLine("Which operation would you like to perform?");
 
-//Console.WriteLine(product);
+Console.WriteLine(@"* (multiply)
+/ (divide)
++ (add)
+- (subtract)
+^2 (square)
+avg (average)");
 
-Console.WriteLine("Would you like to multiply or square the numbers?");
+Console.WriteLine();
 
 string option;
 
 option = Console.ReadLine();
 
-if (option == "multiply")
+int result = 0;
+
+switch (option)
 {
-    foreach (int number in newNumbers)
-    {
-        int result = number * number;
+    case "*":
+        result = (int)(newNumbers[0] * newNumbers[1] * newNumbers[2]);
         Console.WriteLine(result);
-    }
-}
-else if (option == "square")
-{
-    foreach (int number in newNumbers)
-    {
-        int result2 = number * 2;
-        Console.WriteLine(result2);
-    }
-}
-else
-{
-    Console.WriteLine("That is not a valid option");
+        break;
+    case "/":
+        result = (int)(newNumbers[0] / newNumbers[1] / newNumbers[2]);
+        Console.WriteLine(result);
+        break;
+    case "+":
+        result = (int)(newNumbers[0] + newNumbers[1] + newNumbers[2]);
+        Console.WriteLine(result);
+        break;
+    case "-":
+        result = (int)(newNumbers[0] - newNumbers[1] - newNumbers[2]);
+        Console.WriteLine(result);
+        break;
+    case "avg":
+        result = (int)(newNumbers[0] + newNumbers[1] + newNumbers[2] / newNumbers.Count);
+        Console.WriteLine(result);
+        break;
+    case "^2":
+        foreach (int number in newNumbers) { result = number * 2; Console.WriteLine(result); }
+        break;
+    default:
+        Console.WriteLine("Not a valid option");
+        break;
 
 }
